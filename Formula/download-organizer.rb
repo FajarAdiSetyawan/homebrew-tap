e8a576636b4934de8f91ea70106af5617e7315c9
Fajar Adi Setyawan <fajaradisetyawan9@gmail.com>
2026-07-31 08:39:10 +0700

    Update download-organizer to v1.1.2

--- Formula/download-organizer.rb
+++ Formula/download-organizer.rb
@@ -1,10 +1,10 @@
 class DownloadOrganizer < Formula
   desc "Automatically organize your Downloads folder by file type"
   homepage "https://github.com/FajarAdiSetyawan/macOS-Download-Organizer"
-  url "https://github.com/FajarAdiSetyawan/macOS-Download-Organizer/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "8c1a587e2b4439dfda402b5c4f30e2c82148ec7f562d90965e6eeb04ca2c8ebd"
+  url "https://github.com/FajarAdiSetyawan/macOS-Download-Organizer/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "6a103d8ca9f109ae17655375899fd5a0afa3a070308b1292dfd71ab678dc9453"
   license "MIT"
-  version "1.1.1"
+  version "1.1.2"
 
   depends_on xcode: ["14.0", :build]
   depends_on macos: :sonoma
